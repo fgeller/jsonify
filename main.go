14d36286debfef9a13912fbbf286f487dc73b924
Felix Geller <fgeller@gmail.com>
2016-03-14 22:45:35 +1300

    Initial commit.

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr, `
+jsonify [-name value]...
+
+Example:
+
+    jsonify -date "$(date)" -contents main.go
+`)
+}
+
+func main() {
+
+	if len(os.Args) == 1 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	if len(os.Args)%2 == 0 {
+		fmt.Fprintf(os.Stderr, "Expecting even number of arguments.")
+		printUsage()
+		os.Exit(1)
+	}
+
+	var data map[string]string = map[string]string{}
+	for idx := 1; idx < len(os.Args); idx += 2 {
+		name := os.Args[idx][1:]
+		value := os.Args[idx+1]
+		_, err := os.Stat(value)
+		if !os.IsNotExist(err) {
+			bs, err := ioutil.ReadFile(value)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Could not read contents of file %s. err=%s", value, err)
+				os.Exit(1)
+			}
+			value = string(bs)
+		}
+
+		data[name] = value
+	}
+
+	bs, err := json.Marshal(data)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to marshal to json. err=%s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s\n", bs)
+}
